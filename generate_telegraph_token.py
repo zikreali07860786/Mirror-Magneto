d51f64651004a2ef1a2c9f07434336bf8b038025
Abir Hasan <hackitabirhasan@gmail.com>
2020-12-29 14:53:56 +0600

    Update generate_telegraph_token.py

--- generate_telegraph_token.py
+++ generate_telegraph_token.py
@@ -3,4 +3,4 @@ from telegraph import Telegraph
 telegraph = Telegraph()
 telegraph.create_account(short_name=input("Enter a username for your Telegra.ph : "))
 
-print(f"Your Telegra.ph token ==>  {telegraph.get_access_token()}")
+print(f"Your Telegra.ph token ==>  {telegraph.get_access_token()} \n\nBy https://t.me/linux_repo \n")
