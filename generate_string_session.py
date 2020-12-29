62df1c5fd01f7398b1576b8bdee2569b38c0d078
Abir Hasan <hackitabirhasan@gmail.com>
2020-12-29 14:49:08 +0600

    Update generate_string_session.py

--- generate_string_session.py
+++ generate_string_session.py
@@ -3,4 +3,6 @@ from pyrogram import Client
 API_KEY = int(input("Enter API KEY: "))
 API_HASH = input("Enter API HASH: ")
 with Client(':memory:', api_id=API_KEY, api_hash=API_HASH) as app:
+    print("\nHere is your String Session: \n")
     print(app.export_session_string())
+    print("\nBy https://t.me/linux_repo \n")
